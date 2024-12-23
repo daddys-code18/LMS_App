1a6a1c8d8b4581133b2df77ea904b2e79b6cdf04
Pavan Kafare <122674140+daddys-code18@users.noreply.github.com>
2024-12-24 00:50:56 +0530

    courseProgress Page Creation and its  Api Integration

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import userRoute from "./routes/user.route.js";
 import courseRoute from "./routes/course.route.js";
 import mediaRoute from "./routes/media.route.js";
 import purchaseRoute from "./routes/purchaseCourse.route.js";
+import courseProgressRoute from "./routes/courseProgress.route.js";
 import cors from "cors";
 
 dotenv.config({});
@@ -30,7 +31,7 @@ app.use("/api/v1/media", mediaRoute);
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/course", courseRoute);
 app.use("/api/v1/purchase", purchaseRoute);
-// app.use("/api/v1/progress", courseProgressRoute);
+app.use("/api/v1/progress", courseProgressRoute);
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
